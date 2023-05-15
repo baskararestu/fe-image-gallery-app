cd8d5ff28250a38e0c28964638979517d29136d5
baskara restu <baskararw10@gmail.com>
2023-05-15 17:36:58 +0700

    fix home to add infinite page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,24 +1,28 @@
-import React, { useState, useEffect } from "react";
 import axios from "axios";
+import { useEffect, useRef, useState } from "react";
 import { Link } from "react-router-dom";
 
 function Home() {
-  const [data, setData] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
-  const [comment, setComment] = useState("");
-  const token = localStorage.getItem("user_token");
+  const [content, setContent] = useState([]);
   const [showComments, setShowComments] = useState(5); // number of comments to show
-
-  const handleCommentChange = (event) => {
-    setComment(event.target.value);
-    console.log(comment);
-  };
+  const [comment, setComment] = useState("");
+  const [totalPages, setTotalPages] = useState(0);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [isLoading, setIsLoading] = useState(false);
+  const [loadMoreCounter, setLoadMoreCounter] = useState(0);
+  const contentContainerRef = useRef(null);
+  const token = localStorage.getItem("user_token");
 
   const handleShowMoreComments = (contentId) => {
     setShowComments((prevState) => ({
       ...prevState,
       [contentId]: (prevState[contentId] || 0) + 5, // show 5 more comments
     }));
+  };
+
+  const handleCommentChange = (event) => {
+    setComment(event.target.value);
+    console.log(comment);
   };
 
   const handleAddComment = async (contentId) => {
@@ -36,16 +40,16 @@ function Home() {
           },
         }
       );
-      const updatedData = data.map((content) => {
-        if (content.id_content === contentId) {
+      const updatedContent = content.map((item) => {
+        if (item.id_content === contentId) {
           return {
-            ...content,
-            comments: [...content.comments, response.data],
+            ...item,
+            comments: [...item.comments, response.data],
           };
         }
-        return content;
+        return item;
       });
-      setData(updatedData);
+      setContent(updatedContent);
       setComment("");
     } catch (error) {
       console.error(error);
@@ -55,143 +59,152 @@ function Home() {
     }
   };
 
-  // console.log(data, "data");
+  const handleScroll = () => {
+    if (
+      !isLoading &&
+      contentContainerRef.current &&
+      window.innerHeight + window.scrollY >=
+        contentContainerRef.current.offsetHeight
+    ) {
+      setCurrentPage((prevPage) => prevPage + 1);
+      setLoadMoreCounter((prevCounter) => prevCounter + 1);
+    }
+  };
 
   useEffect(() => {
-    const fetchData = async () => {
+    const fetchContent = async () => {
+      setIsLoading(true);
+
       try {
-        const contentsResponse = await axios.get(
-          `http://localhost:8000/content/all-content`
+        const response = await axios.get(
+          `http://localhost:8000/content/infinite-scroll?page=${currentPage}`
         );
-        const contentsData = contentsResponse.data;
-        const combinedData = await Promise.all(
-          contentsData.map(async (content) => {
-            const [commentsResponse, likesResponse] = await Promise.all([
-              axios.get(
-                `http://localhost:8000/content/contents/${content.id_content}/show-comments`
-              ),
-              axios.get(
-                `http://localhost:8000/content/contents/${content.id_content}/show-likes`
-              ),
-            ]);
-            const commentsData = commentsResponse.data;
-            const likesData = likesResponse.data;
+        const { content: newContent, totalPages: newTotalPages } =
+          response.data;
 
-            return {
-              ...content,
-              comments: commentsData,
-              likes: likesData,
-            };
-          })
-        );
-
-        setData(combinedData);
+        setContent((prevContent) => [...prevContent, ...newContent]);
+        setTotalPages(newTotalPages);
+        setIsLoading(false);
+        console.log(response, "response");
       } catch (error) {
         console.error(error);
+        setIsLoading(false);
       }
     };
 
-    fetchData();
-  }, []);
-  console.log(data);
+    fetchContent();
+  }, [currentPage]);
 
-  return (
-    <div className="pt-24 min-h-screen bg-gray-100 p-4">
-      <div className={`grid grid-cols-${data.length} gap-4 justify-center`}>
-        {data.map((content, index) => (
-          <div
-            key={`${content.id_content}-${index}`}
-            className=" bg-white shadow-lg rounded-lg overflow-hidden"
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [isLoading]);
+
+  const renderContent = () => {
+    return content.map((item, index) => (
+      <div
+        key={`${item.id_content}-${index}`}
+        className="bg-white p-4 mb-4 rounded shadow"
+      >
+        <div>
+          <Link
+            key={`${item.id_content}-${index}`}
+            to={`/content-details/${item.id_content}`}
           >
-            <Link
-              key={content.id_content}
-              to={`/content-details/${content.id_content}`}
-            >
-              <img
-                className="w-full h-56 object-cover"
-                src={`http://localhost:8000${content.image}`}
-                alt=""
-              />
-            </Link>
+            <img
+              className="w-full h-56 object-cover"
+              src={`http://localhost:8000${item.image}`}
+              alt=""
+            />
+          </Link>
+        </div>
+        <div className="text-lg font-semibold">{item.caption}</div>
 
-            <div className="p-4">
-              <button className="btn bg-primary-focus text-slate-50 gap-2">
-                Likes
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="h-6 w-6"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
-                  />
-                </svg>
-                {content.likes}
-              </button>
-              <h2 className="text-lg font-medium">{content.caption}</h2>
-              <p className="text-sm text-gray-500 mb-2">
-                Owner: {content.username}
-              </p>
-              <p className="text-sm text-gray-500 mb-2">
-                Created at: {new Date(content.createAt).toLocaleDateString()}
-              </p>
-              <p className="text-sm text-gray-500 mb-2">
-                Comments ({content.comments.length}):
-              </p>
-              <ul className="mb-2">
-                <ul className="mb-2">
-                  {content.comments
-                    .slice(0, showComments[content.id_content] | 5)
-                    .map((comment) => (
-                      <li
-                        key={comment.id_comment}
-                        className="text-sm text-gray-700 mb-1"
-                      >
-                        <span className="font-medium">
-                          {comment.username}:{" "}
-                        </span>
-                        {comment.comment}
-                      </li>
-                    ))}
+        {item.comments && (
+          <div className="mt-4">
+            <h4 className="text-sm font-semibold">
+              Comments ( {item.comments.length} ):
+            </h4>
+            {item.comments
+              .slice(0, showComments[item.id_content] | 5)
+              .map((comment, index) => (
+                <ul key={index} className="flex flex-row gap-2">
+                  <li>{comment.username} :</li>
+                  <li>{comment.comment}</li>
                 </ul>
-
-                <button
-                  className="btn-primary btn-sm text-slate-50 hover:bg-primary-hover"
-                  onClick={() => handleShowMoreComments(content.id_content)}
-                >
-                  Show more comments
-                </button>
-              </ul>
-
-              <form className="flex gap-2">
-                <input
-                  type="text"
-                  placeholder="Add a comment"
-                  className="border-gray-300 rounded-md flex-1 p-2"
-                  onChange={handleCommentChange}
-                />
-                <button
-                  type="submit"
-                  className="btn bg-primary-focus text-slate-50 hover:bg-primary-hover"
-                  onClick={() => {
-                    handleAddComment(content.id_content); // pass contentId to handleAddComment function
-                  }}
-                  disabled={isLoading || !comment} // disable button when loading or when commentText is empty
-                >
-                  {isLoading ? "Commenting..." : "Comment"}
-                </button>
-              </form>
-            </div>
+              ))}
+            <button
+              className="btn-primary btn-sm text-slate-50 hover:bg-primary-hover"
+              onClick={() => handleShowMoreComments(item.id_content)}
+            >
+              Show more comments
+            </button>
+            <form className="flex gap-2">
+              <input
+                type="text"
+                placeholder="Add a comment"
+                className="border-gray-300 rounded-md flex-1 p-2"
+                onChange={handleCommentChange}
+              />
+              <button
+                type="submit"
+                className="btn bg-primary-focus text-slate-50 hover:bg-primary-hover"
+                onClick={() => {
+                  handleAddComment(item.id_content); // pass contentId to handleAddComment function
+                }}
+                disabled={isLoading || !comment} // disable button when loading or when commentText is empty
+              >
+                {isLoading ? "Commenting..." : "Comment"}
+              </button>
+            </form>
           </div>
-        ))}
+        )}
+
+        <p className="mt-4">Likes: {item.likes}</p>
+      </div>
+    ));
+  };
+  console.log(content);
+  return (
+    <div
+      className="pt-16 flex justify-center items-center min-h-screen bg-gray-100"
+      ref={contentContainerRef}
+    >
+      <div className="grid justify-center items-center w-full md:w-2/3 lg:w-1/2 xl:w-1/3 2xl:w-1/4 p-4">
+        {renderContent()}
+        {isLoading && <p className="text-gray-500">Loading...</p>}
       </div>
     </div>
   );
 }
-
 export default Home;
+//   <button
+//     className="btn-primary btn-sm text-slate-50 hover:bg-primary-hover"
+//     onClick={() => handleShowMoreComments(content.id_content)}
+//   >
+//     Show more comments
+//   </button>
+// </ul>
+
+{
+  /* <form className="flex gap-2">
+  <input
+    type="text"
+    placeholder="Add a comment"
+    className="border-gray-300 rounded-md flex-1 p-2"
+    onChange={handleCommentChange}
+  />
+  <button
+    type="submit"
+    className="btn bg-primary-focus text-slate-50 hover:bg-primary-hover"
+    onClick={() => {
+      handleAddComment(content.id_content); // pass contentId to handleAddComment function
+    }}
+    disabled={isLoading || !comment} // disable button when loading or when commentText is empty
+  >
+    {isLoading ? "Commenting..." : "Comment"}
+  </button>
+</form> */
+}
